4fdce0841204f2a12f0a4b3edab33639ed3bd489
Teemu Ranne <teemu.ranne@tuni.fi>
2024-12-04 14:50:49 +0200

    isDate.test.js is implementet

--- test/isDate.test.js
+++ test/isDate.test.js
@@ -1,6 +1,43 @@
 import { expect } from 'chai';
-import add from '../src/isDate.js';
+import isDate from '../src/isDate.js';
 
 describe('isDate', function () {
-  
+    it('Recognize the new date as a date object', () => {
+        expect(isDate(new Date)).to.equal(true)
+      })
+      it('Recognize the first date as a date object', () => {
+        expect(isDate(new Date(0))).to.equal(true)
+      })
+      it('Recognize the day before first date as a date object', () => {
+        expect(isDate(new Date(0 - 1))).to.equal(true)
+      })
+      it('Recognize the last date as a date object', () => {
+        expect(isDate(new Date(8.64e15))).to.equal(true)
+      })
+      it('Recognize the one day over last date as a date object', () => {
+        expect(isDate(new Date(8.64e15 + 1))).to.equal(true)
+      })
+      it('does not recognize text of date as a date object', () => {
+        expect(isDate('Mon April 23 2012')).to.equal(false)
+      })
+      it('Recognize invalid date as a date object', () => {
+        const invalidDate = new Date('invalid date')
+        expect(isDate(invalidDate)).to.equal(true)
+        expect(isNaN(invalidDate.getTime())).to.equal(true)
+      })
+      it('does not recognize number as a date object', () => {
+        expect(isDate(23.2012)).to.equal(false)
+      })
+      it('Does not recognize array as a date object', () => {
+        expect(isDate([])).to.equal(false)
+      })
+      it('Does not recognize function as a date object', () => {
+        expect(isDate(() => {})).to.equal(false)
+      })
+      it('Does not recognize null as a date object', () => {
+        expect(isDate(null)).to.equal(false)
+      })
+      it('Does not recognize undefined as a date object', () => {
+        expect(isDate(undefined)).to.equal(false)
+      })
 });
