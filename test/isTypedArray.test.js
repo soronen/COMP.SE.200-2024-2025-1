619ae9d8337b825ca8c43f70e77c06057237d864
Eetu Soronen <89025008+soronen@users.noreply.github.com>
2024-12-04 13:17:40 +0200

    istypedarray.test.js

--- test/isTypedArray.test.js
+++ test/isTypedArray.test.js
@@ -2,16 +2,25 @@ import { expect } from 'chai';
 import isTypedArray from '../src/isTypedArray.js';
 
 describe('isTypedArray', () => {
-  it('should return true for Float32Array', () => {
-    expect(isTypedArray(new Float32Array(2))).to.be.true;
+
+  it('should return true for Float64Array', () => {
+    expect(isTypedArray(new Float64Array(2))).to.be.true;
   });
 
-  it('should return true for Int8Array', () => {
-    expect(isTypedArray(new Int8Array(2))).to.be.true;
+  it('should return true for Int32Array', () => {
+    expect(isTypedArray(new Int32Array(2))).to.be.true;
   });
 
-  it('should return true for Uint8Array', () => {
-    expect(isTypedArray(new Uint8Array(2))).to.be.true;
+  it('should return true for Uint8ClampedArray', () => {
+    expect(isTypedArray(new Uint8ClampedArray(2))).to.be.true;
+  });
+
+  it('should return true for Uint16Array', () => {
+    expect(isTypedArray(new Uint16Array(2))).to.be.true;
+  });
+
+  it('should return false for ArrayBuffer', () => {
+    expect(isTypedArray(new ArrayBuffer(2))).to.be.false;
   });
 
   it('should return false for regular arrays', () => {
@@ -19,24 +28,16 @@ describe('isTypedArray', () => {
     expect(isTypedArray([1, 2, 3])).to.be.false;
   });
 
-  it('should return for regular allays filled with values', () => {
-    expect(isTypedArray(["hello", 2, false])).to.be.false;
-    expect(isTypedArray([1, 2.555, 3])).to.be.false;
-  });
-
-  it('should return for regular allays filled with the same value', () => {
-    expect(isTypedArray(["hello", "hello"])).to.be.false;
-    expect(isTypedArray([1, 2, 3])).to.be.false;
-  });
-
-  it('should return false for null and undefined', () => {
+  it('should return false for undefined', () => {
     expect(isTypedArray(null)).to.be.false;
     expect(isTypedArray(undefined)).to.be.false;
   });
 
   it('should return false for non-array values', () => {
     expect(isTypedArray({})).to.be.false;
-    expect(isTypedArray('array')).to.be.false;
-    expect(isTypedArray(42)).to.be.false;
+    expect(isTypedArray(() => { })).to.be.false;
+    expect(isTypedArray(new Date())).to.be.false;
+    expect(isTypedArray('notarray')).to.be.false;
+    expect(isTypedArray(true)).to.be.false;
   });
 });
